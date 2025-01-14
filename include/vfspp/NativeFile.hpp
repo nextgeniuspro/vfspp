8a691fea9bfccb0e39523a45543f892650785e7a
S. Messerschmidt <github@smesserschmidt.de>
2025-01-14 12:34:17 +0100

    using binary mode for opening NativeFile

--- include/vfspp/NativeFile.hpp
+++ include/vfspp/NativeFile.hpp
@@ -246,7 +246,7 @@ private:
         m_Mode = mode;
         m_IsReadOnly = true;
         
-        std::ios_base::openmode open_mode = (std::ios_base::openmode)0x00;
+        std::ios_base::openmode open_mode = std::ios_base::binary;
         if ((mode & FileMode::Read) == FileMode::Read) {
             open_mode |= std::fstream::in;
         }
